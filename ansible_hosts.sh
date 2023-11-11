65afe5935fcd1a7a4f86ed3b50dcb3e70951197b
max <maxwellgriffin@ufl.edu>
2023-11-10 19:37:04 -0500

    Now deals with adding a manager to the hosts

--- ansible_hosts.sh
+++ ansible_hosts.sh
@@ -6,14 +6,18 @@ if [ "$#" -eq 0 ]; then
     exit 1
 fi
 
-# Define the inventory file name
+# Define the variables
 inventory_file="/etc/ansible/hosts"
 linux_file="linux.ini"
 win_file="windows.ini"
+man_file="manager.txt"
+got_man=0
+ans="n"
 
 # Create or overwrite the inventory file
 echo -e "[linux_hosts]" > "$linux_file"
 echo -e "\n[windows_hosts]" > "$win_file"
+echo -e "\n[manager]" > "$man_file"
 
 # Add each host to the inventory file with user-provided details
 for host in "$@"; do
@@ -24,14 +28,30 @@ for host in "$@"; do
         l|linux)
 	    read -p "Enter SSH username: " ssh_username
 	    read -p "Enter ip address: " ip_addr
-            echo "$host ansible_host=$ip_addr ansible_user=$ssh_username" >> "$linux_file"
+	    if [ $got_man -eq 0 ]; then
+	       read -p "Is this the Wazuh Manager? (y/n): " ans
+	       if [ $ans -eq "y"]; then
+		  got_man=1
+                  echo "$host ansible_host=$ip_addr ansible_user=$ssh_username" >> "$man_file"
+	       else
+                  echo "$host ansible_host=$ip_addr ansible_user=$ssh_username" >> "$linux_file"
+	       fi
+	    fi
             ;;
         w|windows)
 	    read -p "Enter SSH username: " ssh_username
             read -s -p "Enter SSH password: " ssh_password
 	    echo
 	    read -p "Enter ip address: " ip_addr
-            echo "$host ansible_host=$ip_addr ansible_user=$ssh_username ansible_password=$ssh_password ansible_connection=winrm ansible_winrm_server_cert_validation=ignore" >> "$win_file"
+	    if [ $got_man -eq 0 ]; then
+	       read -p "Is this the Wazuh Manager? (y/n): " ans
+	       if [ $ans -eq "y"]; then
+		  got_man=1
+                  echo "$host ansible_host=$ip_addr ansible_user=$ssh_username" >> "$man_file"
+	       else
+            	  echo "$host ansible_host=$ip_addr ansible_user=$ssh_username ansible_password=$ssh_password ansible_connection=winrm ansible_winrm_server_cert_validation=ignore" >> "$win_file"
+	       fi
+	    fi
             ;;
         *)
             echo "Invalid input. Specify 'l' for Linux or 'w' for Windows."
